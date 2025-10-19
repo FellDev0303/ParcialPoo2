2e449a8fa5d03624296a377ba960ef9c122d6a3f
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 16:58:04 -0500

    Se crea un método en la clase Profesor.java llamada calcularPrestaciones(), la cual calcula las prestaciones correspondientes al 17% del salario total del profesor

--- src/main/java/Modelo/Profesor.java
+++ src/main/java/Modelo/Profesor.java
@@ -8,11 +8,12 @@ package Modelo;
  *
  * @author LENOVO
  */
-public class Profesor extends Persona{
-    int cedula;
-    String areaTrabajo;
-    double salarioHora;
-    double horasDictadas;
+public class Profesor extends Persona {
+
+    private int cedula;
+    private String areaTrabajo;
+    private double salarioHora;
+    private double horasDictadas;
 
     public Profesor(int cedula, String areaTrabajo, double salarioHora, double horasDictadas, String nombre, int edad, String direccion, String telefono, String nacimiento) {
         super(nombre, edad, direccion, telefono, nacimiento);
@@ -21,16 +22,24 @@ public class Profesor extends Persona{
         this.salarioHora = salarioHora;
         this.horasDictadas = horasDictadas;
     }
-    
-    public double calcularSalario(){
-     return salarioHora * horasDictadas ;
+
+    public double calcularSalario() {
+        return salarioHora * horasDictadas;
+    }
+
+    public double calcularSalario(double incentivo) {
+        return (salarioHora * horasDictadas) + incentivo;
     }
     
-    @Override 
-    public String toString(){
-    return super.toString()
-            + "\n Cédula: " + cedula
-            + "\n Área de trabajo :" + areaTrabajo
-            + "\n Salario base: $" + calcularSalario();
+    public double calcularPrestaciones(){
+    return calcularSalario()*0.17;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + "\n Cédula: " + cedula
+                + "\n Área de trabajo :" + areaTrabajo
+                + "\n Salario base: $" + calcularSalario();
     }
 }
