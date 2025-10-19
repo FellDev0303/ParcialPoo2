cd0759845a8e61cada3d4fa067ad6ed11cd678ed
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 17:50:51 -0500

    Se crea la clase EstudiantePresencial.java con propositos de herencia c:

---
+++ src/main/java/Modelo/EstudiantePresencial.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class EstudiantePresencial extends Estudiante {
+
+    public EstudiantePresencial(String codigo, double promedio, String nombre, int edad, String direccion, String telefono, String nacimiento) {
+        super(codigo, promedio, nombre, edad, direccion, telefono, nacimiento);
+    }
+    
+}
