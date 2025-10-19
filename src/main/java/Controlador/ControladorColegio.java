36c161f3e5cb7ee7c78fd8118f14d1c457c7f585
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 16:23:12 -0500

    Se crea el método registrarProfesor() que solicita todos los datos de este y crea un "objeto p" / profesor el cual agrega a un ArrayList

--- src/main/java/Controlador/ControladorColegio.java
+++ src/main/java/Controlador/ControladorColegio.java
@@ -13,6 +13,23 @@ import javax.swing.JOptionPane;
  * @author LENOVO
  */
 public class ControladorColegio {
+    private ArrayList<Profesor> profesores=  new ArrayList<>();
+    private ArrayList<Estudiante> estudiantes = new ArrayList<>();
     
+    public void registrarProfesor(){
+    String nombre= JOptionPane.showInputDialog("Ingrese el nombre del profesor: ");
+    int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad del profesor: "));
+    String direccion = JOptionPane.showInputDialog("Ingrese la dirección del profesor: ");
+    String telefono = JOptionPane.showInputDialog("Ingrese el # de teléfono del profesor:");
+    String nacimiento = JOptionPane.showInputDialog("Ingrese la fecha de nacimiento: ");
+    int cedula = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la Cédula del profesor:  "));
+    String area= JOptionPane.showInputDialog("En que área trabaja el profesor?: ");
+    double salario = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el salario por hora: "));
+    double horas = Double.parseDouble(JOptionPane.showInputDialog("Ingrese las horas dictadas: "));
+    
+    Profesor p = new Profesor(cedula, area, salario, horas, nombre, edad, direccion, telefono, nacimiento);
+    profesores.add(p);
+    JOptionPane.showInputDialog("Profesor Registrado correctamente.");
+    }
     
 }
