b157149b135eb35d800aaffe777523e52cb7a6a5
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 17:55:41 -0500

    En la clase EstudianteVirtual.java se crea el método Override obtenerModalidad que devuelve el String "Modalidad : Virtual"

--- src/main/java/Modelo/EstudianteVirtual.java
+++ src/main/java/Modelo/EstudianteVirtual.java
@@ -13,5 +13,9 @@ public class EstudianteVirtual extends Estudiante {
     public EstudianteVirtual(String codigo, double promedio, String nombre, int edad, String direccion, String telefono, String nacimiento) {
         super(codigo, promedio, nombre, edad, direccion, telefono, nacimiento);
     }
-    
+
+    @Override
+    public String obtenerModalidad() {
+        return "Modalidad: Virtual";
+    }
 }
