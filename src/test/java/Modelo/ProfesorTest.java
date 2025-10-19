55d7af40e49a35723b8028a209648fc954c41e34
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 18:08:00 -0500

    se completa el código del test

--- src/test/java/Modelo/ProfesorTest.java
+++ src/test/java/Modelo/ProfesorTest.java
@@ -16,6 +16,23 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author LENOVO
  */
 public class ProfesorTest {
+        public static void main(String[] args) {
+        Profesor profe = new Profesor(12345, "Matemáticas", 50000, 40, "Verónica", 35, "Calle 10 #23-45", "3201234567", "15/03/1989");
+
+        double salarioEsperado = 50000 * 40;
+        double salarioCalculado = profe.calcularSalario();
+        double prestacionesEsperadas = salarioEsperado * 0.17;
+
+        System.out.println("==== TEST PROFESOR ====");
+        System.out.println("Salario esperado: " + salarioEsperado + " | Calculado: " + salarioCalculado);
+        System.out.println("Prestaciones esperadas: " + prestacionesEsperadas + " | Calculadas: " + profe.calcularPrestaciones());
+
+        if (salarioEsperado == salarioCalculado && prestacionesEsperadas == profe.calcularPrestaciones()) {
+            System.out.println(" Test Profesor PASADO");
+        } else {
+            System.out.println(" Test Profesor FALLÓ");
+        }
+    }
 
     
 }
