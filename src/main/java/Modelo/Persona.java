36f5f28d9ad914d0b05a4595b4bc2646eafd3016
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 13:42:27 -0500

    Se crea la clase "Persona.java" con fines de herencia para "Estudiante y Profesor" (se crearán estas clases en un futuro).

---
+++ src/main/java/Modelo/Persona.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Persona {
+    int nombre;
+    int edad;
+    int direccion;
+    int telefono;
+    int nacimiento;
+
+    public Persona(int nombre, int edad, int direccion, int telefono, int nacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nacimiento = nacimiento;
+    }
+    
+    
+    
+    
+}
