f9f757c980aeba04253b5e8092e9448b3f0cd4ee
FellDev0303 <josejose2868@hotmail.com>
2025-10-19 15:18:37 -0500

    Se crea la clase profesor

---
+++ src/main/java/Modelo/Profesor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Profesor extends Persona{
+    int cedula;
+    String areaTrabajo;
+    double salarioHora;
+    double horasDictadas;
+
+    public Profesor(int cedula, String areaTrabajo, double salarioHora, double horasDictadas, int nombre, int edad, int direccion, int telefono, int nacimiento) {
+        super(nombre, edad, direccion, telefono, nacimiento);
+        this.cedula = cedula;
+        this.areaTrabajo = areaTrabajo;
+        this.salarioHora = salarioHora;
+        this.horasDictadas = horasDictadas;
+    }
+    
+    
+    
+    
+}
